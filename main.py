96af68ca4f12fb345057f4512fe339ea7a289348
Patrik Jankuv <jankupat@fel.cvut.cz>
2020-05-18 18:07:19 +0200

    start work on input

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-board = [[" ", " ", " "],
-         [" ", " ", " "],
-         [" ", " ", " "]]
+board = [["1", "2", "3"],
+         ["4", "5", "6"],
+         ["7", "8", "9"]]
+
+game_in_progress = True
 
 
 def print_board(board):
@@ -10,10 +12,31 @@ def print_board(board):
         print("-------------")
 
 
-def next_turn(board, item, x, y):
+def set_position(board, item, x, y):
     board[x][y] = item
 
+# todo 
+def input_turn(chart):
+    temp = -1
+    while (temp > 1 & temp < 9):
+        temp = input("Choose a position from 1-9: ")
+        temp = int(temp) - 1
 
-print('Hello in tic-tac-toe')
-board[0][0] = "X"
-print_board(board)
+    print(temp)
+    # if(temp == 1):
+        
+
+
+def game_loop():
+    while(game_in_progress):
+        print("Player1 [x] on the turn")
+        
+        print_board(board)
+
+    
+
+print('Hello in tic-tac-toe\n')
+input_turn("X")
+# game_loop()
+
+
